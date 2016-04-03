b363fc9a618ef437aaccd6fa7b939be0a7644abe
Luiz Romário Santana Rios <luizromario@gmail.com>
2016-04-03 09:39:07 -0300

    Bump Copyright year

--- qtmvt.hpp
+++ qtmvt.hpp
@@ -1,5 +1,5 @@
 /*  Qt Model/View Templates
-    Copyright (c) 2015 Luiz Romário Santana Rios <luizromario@gmail.com>
+    Copyright (c) 2015--2016 Luiz Romário Santana Rios <luizromario@gmail.com>
     All rights reserved.
 
     Redistribution and use in source and binary forms, with or without
